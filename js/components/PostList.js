065c5c36203ed585dad63435383d68f3af238a8d
Niklas Lindgren <niklas@sc5.io>
2016-05-25 12:25:34 +0300

    Add pagination

--- js/components/PostList.js
+++ js/components/PostList.js
@@ -7,6 +7,31 @@ const first = (arr) => arr[0];
 const last = (arr) => arr[arr.length - 1];
 
 class PostList extends React.Component {
+    rewindPageCursor() {
+        const { relay, viewer: { posts } } = this.props;
+
+        const beforeCursor = first(posts.edges).cursor;
+        const afterCursor = null;
+
+        relay.setVariables({ afterCursor, beforeCursor });
+    }
+
+    advancePageCursor() {
+        const { relay, viewer: { posts } } = this.props;
+
+        const beforeCursor = null;
+        const afterCursor = last(posts.edges).cursor;
+
+        relay.setVariables({ afterCursor, beforeCursor });
+    }
+
+    increasePageSize() {
+        const { relay } = this.props;
+        const pageSize = relay.variables.pageSize + 10;
+
+        relay.setVariables({ pageSize }, (args) => console.log("ORSC", args));
+    }
+
     renderPostsList() {
         const { viewer: { posts } } = this.props;
 
@@ -19,10 +44,32 @@ class PostList extends React.Component {
                 <Link to={`/post/${p.id}`}>{p.title}</Link></li>)}</ol>;
     }
 
+    renderPager() {
+        const { viewer: { posts: { pageInfo } } } = this.props;
+
+        if (!pageInfo.hasNextPage && !pageInfo.hasPreviousPage) {
+            return null;
+        }
+
+        // return (
+        //     <div>
+        //         <button onClick={() => this.increasePageSize()}>Show more</button>
+        //     </div>
+        // );
+
+        return (
+            <div>
+            {pageInfo.hasPreviousPage && <button onClick={() => this.rewindPageCursor()}>Previous page</button>}
+            {pageInfo.hasNextPage && <button onClick={() => this.advancePageCursor()}>Next page</button>}
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
             {this.renderPostsList()}
+            {this.renderPager()}
             </div>
         );
     }
@@ -30,13 +77,20 @@ class PostList extends React.Component {
 
 export default Relay.createContainer(PostList, {
     initialVariables: {
-        pageSize: 10
+        pageSize: 10,
+        afterCursor: null,
+        beforeCursor: null
     },
     fragments: {
         viewer: () => Relay.QL`
             fragment on Blog {
-                posts(first: $pageSize) {
+                posts(first: $pageSize, after: $afterCursor) {
+                    pageInfo {
+                        hasPreviousPage
+                        hasNextPage
+                    }
                     edges {
+                        cursor
                         node {
                             id
                             title
